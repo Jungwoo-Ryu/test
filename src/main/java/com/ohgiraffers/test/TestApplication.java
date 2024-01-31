a226cb8bb5714c1bbb4a048bcb017bf191491d9d
Ken <jryu1521@gmail.com>
2024-01-31 13:09:01 +0900

    [test] Program test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+    //
+    // 여기는 주석입니다.
+    /*
+        여기는
+        여러줄
+        주석 입니다.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("나는 개발자입니다."); // cmd + d
+        
+    }
+
+}
