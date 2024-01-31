5b3b2265b82de461a66213f21c2f4f80b0d92a7d
kenryu <jryu1521@gmail.com>
2024-01-31 14:31:56 +0900

    [test] Program test

--- src/main/java/com/ohgiraffers/test/TestApplication.java
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -11,7 +11,6 @@ public class TestApplication {
 
     public static void main(String[] args) {
         System.out.println("나는 개발자입니다."); // cmd + d
-        System.out.println("나는 개발자입니다."); // cmd + d
 
     }
 
